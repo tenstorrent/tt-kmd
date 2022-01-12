c6e8b6cfd8fe4748479caff46044424840005b97
Aaron Fong <afong@tenstorrent.com>
2022-01-12 17:08:00 -0500

    TTKMD v1.12

--- module.c
+++ module.c
@@ -6,7 +6,7 @@
 #include "chardev.h"
 #include "enumerate.h"
 
-#define TTDRIVER_VER "1.11.1"
+#define TTDRIVER_VER "1.12"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Tenstorrent AI kernel driver");

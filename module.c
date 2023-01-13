1487244b169485ac8c16cec1ebf8abc9b15e5d85
Andrew Lewycky <alewycky@tenstorrent.com>
2023-01-13 13:28:26 -0500

    TTKMD v1.18

--- module.c
+++ module.c
@@ -6,7 +6,7 @@
 #include "chardev.h"
 #include "enumerate.h"
 
-#define TTDRIVER_VER "1.17"
+#define TTDRIVER_VER "1.18"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Tenstorrent AI kernel driver");

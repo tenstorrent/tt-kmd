610642a140fcd2e805e3f7a49483c46194a3344f
Tenstorrent Releases <releases@tenstorrent.com>
2025-12-04 00:39:08 +0000

    TTKMD v2.6.0-rc1

--- module.h
+++ module.h
@@ -8,9 +8,9 @@
 #include <linux/pci.h>
 
 #define TENSTORRENT_DRIVER_VERSION_MAJOR 2
-#define TENSTORRENT_DRIVER_VERSION_MINOR 5
-#define TENSTORRENT_DRIVER_VERSION_PATCH 1
-#define TENSTORRENT_DRIVER_VERSION_SUFFIX "-pre"
+#define TENSTORRENT_DRIVER_VERSION_MINOR 6
+#define TENSTORRENT_DRIVER_VERSION_PATCH 0
+#define TENSTORRENT_DRIVER_VERSION_SUFFIX "-rc1"
 
 // Module options that need to be passed to other files
 extern uint dma_address_bits;
